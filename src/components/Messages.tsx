d124d79e50545230deb0f51b1a8f9edbf19726c1
nadavmoreno <nadavmoreno@gmail.com>
2024-08-18 13:01:03 +0300

    fix no content message view

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -24,59 +24,64 @@ const Messages = forwardRef<HTMLDivElement, MessagesProps>(
         onScroll={onScroll}
         ref={ref}
       >
-        {messages.map((message) => (
-          <div key={message.id}>
-            {message.isFirstQuestion === false && (
-              <hr className="h-[1px] border-line left-3 relative w-[calc(100%_+_1.5rem)] my-3" />
-            )}
-            <div
-              className={`text-sm p-3 mb-2 ${
-                [MessageType.StartBot, MessageType.Bot].includes(message.type)
-                  ? "bg-message-bot-background text-message-bot-foreground rounded-[10px_10px_10px_0] mr-6"
-                  : "bg-message-user-background text-message-user-foreground rounded-[10px_10px_0_10px] ml-[3.8rem]"
-              }`}
-            >
-              {message.type === MessageType.Bot ? (
-                <Markdown className="markdown">{message.content}</Markdown>
-              ) : (
-                message.content
-              )}
-            </div>
-
-            {message.type === MessageType.Bot && (
-              <>
-                {message.links && (
-                  <div className="bg-message-bot-background text-message-bot-foreground border-r-4 border-message-user-background pr-[5px] py-2 mb-[6px]">
-                    <span className="text-sm font-bold text-input-placholder mb-1 inline-block">
-                      {globalConfigObject?.slugs?.returning_links_title}
-                    </span>
-                    <ul className="list-inside">
-                      {message.links.map((link, index) => (
-                        <li
-                          key={index}
-                          className="text-sm mb-1 list-image-[url(assets/arrow-left.svg)]"
-                        >
-                          <a
-                            href={link.url}
-                            target="_blank"
-                            className="text-sm mr-[2px] text-input-placholder"
-                          >
-                            {link.title}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
+        {messages.map(
+          (message) =>
+            message.content && (
+              <div key={message.id}>
+                {message.isFirstQuestion === false && (
+                  <hr className="h-[1px] border-line left-3 relative w-[calc(100%_+_1.5rem)] my-3" />
                 )}
-                <Rating
-                  message={message}
-                  setMessages={setMessages}
-                  globalConfigObject={globalConfigObject}
-                />
-              </>
-            )}
-          </div>
-        ))}
+                <div
+                  className={`text-sm p-3 mb-2 ${
+                    [MessageType.StartBot, MessageType.Bot].includes(
+                      message.type,
+                    )
+                      ? "bg-message-bot-background text-message-bot-foreground rounded-[10px_10px_10px_0] mr-6"
+                      : "bg-message-user-background text-message-user-foreground rounded-[10px_10px_0_10px] ml-[3.8rem]"
+                  }`}
+                >
+                  {message.type === MessageType.Bot ? (
+                    <Markdown className="markdown">{message.content}</Markdown>
+                  ) : (
+                    message.content
+                  )}
+                </div>
+
+                {message.type === MessageType.Bot && (
+                  <>
+                    {message.links && (
+                      <div className="bg-message-bot-background text-message-bot-foreground border-r-4 border-message-user-background pr-[5px] py-2 mb-[6px]">
+                        <span className="text-sm font-bold text-input-placholder mb-1 inline-block">
+                          {globalConfigObject?.slugs?.returning_links_title}
+                        </span>
+                        <ul className="list-inside">
+                          {message.links.map((link, index) => (
+                            <li
+                              key={index}
+                              className="text-sm mb-1 list-image-[url(assets/arrow-left.svg)]"
+                            >
+                              <a
+                                href={link.url}
+                                target="_blank"
+                                className="text-sm mr-[2px] text-input-placholder"
+                              >
+                                {link.title}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                    <Rating
+                      message={message}
+                      setMessages={setMessages}
+                      globalConfigObject={globalConfigObject}
+                    />
+                  </>
+                )}
+              </div>
+            ),
+        )}
         {isLoading && <TypingIndicator />}
       </div>
     );
