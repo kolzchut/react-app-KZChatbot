e4bacb24a7579d052dc959dda1911c740c6a8b2c
drorsnir <FreedomFighterSparrow@gmail.com>
2025-12-09 15:59:43 +0200

    Push full event name to dataLayer instead of 'chatbot' and action param

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -3,15 +3,15 @@ declare const window: Window & { dataLayer: Record<string, unknown>[]; };
 export const pushAnalyticsEvent = (eventAction: string, eventLabel: string | null = null, source?: string) => {
     window.dataLayer = window.dataLayer || [];
     const eventData: Record<string, unknown> = {
-        event: "chatbot",
+        event: "chatbot_" + eventAction,
         event_action: eventAction,
         event_label: eventLabel
     };
-    
+
     if (source) {
         eventData.source = source;
     }
-    
+
     window.dataLayer.push(eventData);
 
 };
